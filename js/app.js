f11b3011a3c98828d5885a7ff717ed7311303352
Nicolas Fillard <nicolasfillard@macbook-air-de-nicolas.home>
2017-09-10 17:18:18 +0200

    maquette responsive

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $('.slider').slick({
+      vertical: true,
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1
+    });
+});
